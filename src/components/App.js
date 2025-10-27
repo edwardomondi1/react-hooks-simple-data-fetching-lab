0cfbc85ef4b8b46f7ad052925428af03874681a1
edwardomondi1 <edward.omondi1@student.moringaschool.com>
2025-10-27 12:32:07 +0300

    message

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={imageUrl} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
